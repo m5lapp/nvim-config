9a9812e2cfa4e861311b3d274da8c18742430569
m5lapp <git@m5l.app>
2023-05-14 12:27:53 +0100

    Added default Tree-sitter plugins and cleaned up plugins file

--- plugins.lua
+++ plugins.lua
@@ -1,21 +1,41 @@
 local plugins = {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "json", "proto", "yaml",                 -- Data formats
+        "sql",                                   -- Databases
+        "latex", "markdown",                     -- Documents
+        "git_config", "gitcommit", "gitignore",  -- Git
+        "go", "gomod", "gosum",                  -- Go
+        "python",                                -- Python
+        "bash", "cmake",                         -- Systems
+        "html", "http", "javascript",            -- Web development
+      },
+    },
+  },
+
+  {
     "williamboman/mason.nvim",
-      opts = {
-        ensure_installed = {
+    opts = {
+      ensure_installed = {
         "goimports",
         "golines",
         "gopls",
       },
     },
   },
+
   {
+    -- Override the nvim-lspconfig package with a new configuration that loads
+    -- in the default NvChad lspconfig and our custom lspconfig.
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
   },
+
   {
     "jose-elias-alvarez/null-ls.nvim",
     ft = "go",
@@ -23,6 +43,7 @@ local plugins = {
       return require "custom.configs.null-ls"
     end,
   },
+
   {
     "olexsmir/gopher.nvim",
     ft = "go",
