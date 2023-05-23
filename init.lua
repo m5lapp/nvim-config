21d1fb6f60c024b1910f926d1871577abb93d32d
m5lapp <git@m5l.app>
2023-05-23 11:53:28 +0100

    Added configuration for default tab sizes and line numbers.

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 vim.opt.colorcolumn = "81,101,121"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "auto"
+vim.opt.spell = true
 vim.opt.tabstop = 4
-
+vim.opt.wrap = false
